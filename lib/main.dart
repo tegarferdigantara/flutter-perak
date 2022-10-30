9920968a79cdf5aa8e73eaa22edc9bc35a882091
Tegar Ferdigantara <tegarferdigantara007@student.untan.ac.id>
2022-10-31 01:05:33 +0700

    Navigator

--- lib/main.dart
+++ lib/main.dart
@@ -1,135 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_3/home_page.dart';
-import 'package:flutter_application_3/main.dart';
-import 'package:flutter_application_3/register_page.dart';
-
+import 'ui/my_home_page.dart';
+import 'ui/login_page.dart';
+import 'ui/register_page.dart';
+import 'ui/home_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Event Org',
+      title: 'Starter Template',
       theme: ThemeData(
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity),
-      home: Scaffold(
-        // appBar: AppBar(
-        //   title: const Text(
-        //     'EVENT ORG',
-        //     style: TextStyle(fontWeight: FontWeight.bold),
-        //   ),
-        // ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(
-                height: 25,
-              ),
-              const Text(
-                'Halo User',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const Text('Selamat datang kembali', style: TextStyle(fontSize: 20)),
-              const SizedBox(
-                height: 20,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.grey[200],
-                      borderRadius: BorderRadius.circular(12),
-                      border: Border.all(color: Colors.white)),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 20.0),
-                    child: TextField(
-                      decoration: const InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Email',
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.grey[200],
-                      borderRadius: BorderRadius.circular(12),
-                      border: Border.all(color: Colors.white)),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 20.0),
-                    child: TextField(
-                      obscureText: true,
-                      decoration: const InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Password',
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Container(
-                    padding: EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                        color: Colors.green[100]),
-                    child: Center(
-                      child: Text('Sign In',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20)),
-                    )),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Not a member?',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    ' Register Now!',
-                    style: TextStyle(
-                        color: Colors.blue, fontWeight: FontWeight.bold),
-                  )
-                ],
-              )
-            ],
-          ),
-        ),
-        // bottomNavigationBar: BottomNavigationBar(
-        //   items: const [
-        //     BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-        //     BottomNavigationBarItem(
-        //         icon: Icon(Icons.notifications), label: 'Notification'),
-        //     BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profil')
-        //   ],
-        // ),
+        primarySwatch: Colors.blue,
       ),
+      // home: const LoginPage(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => LoginPage(),
+        '/homePage': (context) => const HomePage(),
+        // '/loginPage': (context) => SecondScreenWithData(),
+        // '/returnDataScreen': (context) => const ReturnDataScreen(),
+        // '/replacementScreen': (context) => const ReplacementScreen(),
+        // '/anotherScreen': (context) => const AnotherScreen(),
+      },
     );
   }
 }
